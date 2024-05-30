b29b642889ac0765544942d89d8406cf7228cdf0
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2024-05-31 01:09:00 +0300

    Add files via upload

---
+++ Rentogo Properties_files/index.js
@@ -0,0 +1,16 @@
+lottie.loadAnimation({
+    container: document.getElementById('zaparevideo'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'zapbanner.json' // the path to the animation json
+});
+
+lottie.loadAnimation({
+  container: document.getElementById('zapareeqry'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  // path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json' // the path to the animation json
+  path: 'zapenqry.json' // the path to the animation json
+});
